8aca47d491a65690d325fdde52ece749e03f80bf
samuse-va <vik-sam@yandex.ru>
2022-06-05 20:59:19 +0300

    calculate hash

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,3 +1,16 @@
+import crypto from "crypto";
+import fs from "fs";
+
 export const calculateHash = async () => {
-    // Write your code here 
+  const file = await fs.promises.readFile(
+    "./files/fileToCalculateHashFor.txt",
+    "utf-8"
+  );
+  const hash = crypto.createHash("sha256");
+  hash.update(file);
+
+  const hex = hash.digest("hex");
+  console.log(hex);
 };
+
+calculateHash();
