3aaf1b1b300beac9a5c9154b6f3eca9ce717dc91
samuse-va <vik-sam@yandex.ru>
2022-06-05 23:17:47 +0300

    delete comments

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,21 +1,9 @@
 import fs from "fs";
 export const create = async () => {
   try {
-    // await fs.promises.access("./fresh.txt");
-    // console.log("access");
     await fs.promises.writeFile("./fresh.txt", "I am fresh and young");
-    // if (file == undefined) {
-    //   console.error("FS operation failed");
-    //   // const error = new Error("FS operation failed");
-    //   // throw error;
-    // } else {
-    //   await fs.promises.writeFile("./fresh.txt", "I am fresh and young");
-    // }
   } catch (error) {
     console.error("FS operation failed");
   }
-  // finally {
-  //   await fs.promises.writeFile("./fresh.txt", "I am fresh and young");
-  // }
 };
 create();
